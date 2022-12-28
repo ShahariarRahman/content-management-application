dc1060a33b0842fabb461f7c5f4dee5646a82ce5
ShahariarRahman <shahariarrahman98@gmail.com>
2022-12-29 05:20:53 +0600

    Initial step up for layout and it's page

--- _project_steps.js
+++ _project_steps.js
@@ -1,16 +1,28 @@
 /* 
-1. npx create-react-app content-management-application
-2. create git repository and push the initial code
-3. start taking log for every step and then push the code to github
-4. In src create folders > layout, pages, components, routes, thunk
-5. 
-6. 
-7. 
-8. 
-9. 
-10. 
-11. 
-12. 
-13. 
+initial step up:
+    1. npx create-react-app content-management-application
+    2. create git repository and push the initial code
+    3. start taking log for every step and then push the code to github
+    4. In src create folders> layout, pages, components, routes, thunk
+    5. pages, layout> (folders) Main, Others
+    6. Main> Home.js
+    7. 
+    8. 
+    9. 
+
+routes step up:
+    1. npm install react-router-dom
+    2. 
+    3. 
+    4. 
+    5. 
+    6. 
+    7. 
+    8. 
+    9. 
+    10. 
+    11. 
+    12. 
+    13. 
 
 */

91965373a8ae1ca4e155f1df3504513d84ac3149
ShahariarRahman <shahariarrahman98@gmail.com>
2022-12-29 04:53:20 +0600

    Taking log for every step change in the code

---
+++ _project_steps.js
@@ -0,0 +1,16 @@
+/* 
+1. npx create-react-app content-management-application
+2. create git repository and push the initial code
+3. start taking log for every step and then push the code to github
+4. 
+5. 
+6. 
+7. 
+8. 
+9. 
+10. 
+11. 
+12. 
+13. 
+
+*/

8256755360b71da34ba36976195ee9c494efb62a
ShahariarRahman <shahariarrahman98@gmail.com>
2022-12-29 05:05:47 +0600

    In src create folders> layout, pages, components, routes, thunk

--- _project_steps.js
+++ _project_steps.js
@@ -2,7 +2,7 @@
 1. npx create-react-app content-management-application
 2. create git repository and push the initial code
 3. start taking log for every step and then push the code to github
-4. 
+4. In src create folders > layout, pages, components, routes, thunk
 5. 
 6. 
 7. 

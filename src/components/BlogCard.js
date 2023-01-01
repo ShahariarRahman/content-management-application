825ad03a482a04ab8890cbe33e18c01bad3c63e6
ShahariarRahman <shahariarrahman98@gmail.com>
2023-01-01 12:04:02 +0600

    blog card ready dynamically

--- src/components/BlogCard.js
+++ src/components/BlogCard.js
@@ -1,28 +1,28 @@
 import React from 'react';
+import { MdEmojiObjects } from 'react-icons/md';
 
 const BlogCard = ({ blog }) => {
+    const { _id, date, description, img, name, tags, title, } = blog || {};
     return (
         <div className="max-w-lg w-full lg:max-w-full lg:flex">
             <div className="border border-gray-400 bg-white p-4 flex flex-col justify-between leading-normal rounded-lg">
                 <div className="mb-8">
-                    <p className="text-sm text-gray-600 flex items-center">
-                        <svg className="fill-current text-gray-500 w-3 h-3 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                            <path d="M4 8V6a6 6 0 1 1 12 0v2h1a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-8c0-1.1.9-2 2-2h1zm5 6.73V17h2v-2.27a2 2 0 1 0-2 0zM7 6v2h6V6a3 3 0 0 0-6 0z" />
-                        </svg>
-                        Members only
-                    </p>
-                    <div className="text-gray-900 font-bold text-xl mb-2">Can coffee make you a better developer?</div>
-                    <p className="text-gray-700 text-base">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.</p>
+                    <div className='flex items-center'>
+                        < MdEmojiObjects />
+                        <p className="ml-2 text-sm text-gray-600 flex items-center">{tags || "Topics"}</p>
+                    </div>
+                    <div className="text-gray-900 font-bold text-xl mb-2">{title || "ReactJS is better than other javaScript library library"}</div>
+                    <p className="text-gray-700 text-base">{description || "Compared to other frontend frameworks, the React code is easier to maintain and is flexible due to its modular structure. This flexibility, in turn, saves huge amount of time and cost to businesses."}</p>
                 </div>
                 <div className="flex flex-row-reverse items-center justify-between">
                     <div className='flex items-center'>
                         <div className="w-10 h-10 bg-black rounded-full mr-4 grid place-content-center overflow-hidden">
-                            <img className="w-16 h-16 object-contain" src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?w=100&h=100" alt="Avatar of Jonathan Reinink" />
+                            <img className="w-16 h-16 object-contain" src={img || "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?w=100&h=100"} alt="Avatar of Jonathan Reinink" />
                         </div>
                         <div className="text-sm">
                             <div>
-                                <p className="text-gray-900 leading-none">Jonathan Reinink</p>
-                                <p className="text-gray-600">Aug 18</p>
+                                <p className="text-gray-900 leading-none">{name || "Md. Shahariar Rahman"}</p>
+                                <p className="text-gray-600">{date || "2023-jan-01"}</p>
                             </div>
                         </div>
                     </div>

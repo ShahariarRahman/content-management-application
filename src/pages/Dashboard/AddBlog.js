ebd21801807380954c98004b7dfd94b79b416e1b
ShahariarRahman <shahariarrahman98@gmail.com>
2023-01-02 06:09:42 +0600

    add blog: text field recorrect

--- src/pages/Dashboard/AddBlog.js
+++ src/pages/Dashboard/AddBlog.js
@@ -3,112 +3,92 @@ import React from 'react';
 const AddBlog = () => {
     return (
         <main className="main flex flex-col flex-grow -ml-64 md:ml-0 transition-all duration-150 ease-in bg-gray-300">
-            <body class="font-mono">
-                <div class="container mx-auto">
-                    <div class="flex justify-center px-6 mt-2">
-                        <div class="w-full lg:w-11/12 flex">
-                            <div class="w-full bg-white p-5 rounded-lg lg:rounded-l-none">
-                                <h3 class="pt-4 text-2xl text-center">Publish a Blog!</h3>
-                                <form class="px-8 pt-6 pb-8 mb-4 bg-white rounded">
-                                    <div class="mb-4 md:flex md:justify-between">
-                                        <div class="mb-4 md:mr-2 md:mb-0">
-                                            <label class="block mb-2 text-sm font-bold text-gray-700" for="firstName">Author Name</label>
+            <div className="container mx-auto">
+                <div className="flex justify-center px-6 mt-8">
+                    <div className="w-full max-w-4xl flex">
+                        <div className="w-full bg-white p-5 rounded-lg">
+                            <h3 className="pt-4 text-2xl text-center font-semibold">Publish a Blog!</h3>
+                            <form className="px-8 pt-6 mb-4 bg-white rounded">
+                                <div className="mb-4 md:flex md:justify-between">
+                                    <div>
+                                        <div className="mb-4 md:mr-2">
+                                            <label className="block mb-2 text-sm font-bold text-gray-700" for="firstName">Title:</label>
                                             <input
-                                                class="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                                                className="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
                                                 id="firstName"
                                                 type="text"
-                                                placeholder="First Name"
+                                                placeholder="Blog Title"
                                             />
                                         </div>
-                                        <div class="mb-4 md:mr-2 md:mb-0">
-                                            <label class="block mb-2 text-sm font-bold text-gray-700" for="firstName">Author Name</label>
+                                        <div className="mb-4 md:mr-2 md:mb-0">
+                                            <label className="block mb-2 text-sm font-bold text-gray-700" for="firstName">Topics:</label>
+                                            <div className='select-none text-sm'>
+                                                <input type="checkbox" name="reactJS" id='reactJS' className='mx-2' />
+                                                <label htmlFor="reactJS">ReactJS</label>
+                                                <input type="checkbox" name="javaScript" id='javaScript' className='mx-2' />
+                                                <label htmlFor="javaScript">JavaScript</label>
+                                                <input type="checkbox" name="html" id='html' className='mx-2' />
+                                                <label htmlFor="html">Html</label>
+                                                <input type="checkbox" name="css" id='css' className='mx-2' />
+                                                <label htmlFor="css">Css</label>
+                                                <input type="checkbox" name="mongodb" id='mongodb' className='mx-2' />
+                                                <label htmlFor="mongodb">MongoDb</label>
+                                                <input type="checkbox" name="expressJS" id='expressJS' className='mx-2' />
+                                                <label htmlFor="expressJS">ExpressJS</label>
+                                                <input type="checkbox" name="nodeJS" id='nodeJS' className='mx-2' />
+                                                <label htmlFor="nodeJS">NodeJS</label>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <div className="mb-4 md:mr-2">
+                                            <label className="block mb-2 text-sm font-bold text-gray-700" for="firstName">Author Name</label>
                                             <input
-                                                class="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                                                className="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
                                                 id="firstName"
                                                 type="text"
-                                                placeholder="First Name"
+                                                placeholder="Your Name"
                                             />
                                         </div>
-                                        <div class="md:ml-2">
-                                            <label class="block mb-2 text-sm font-bold text-gray-700" for="lastName">
-                                                Last Name
+                                        <div>
+                                            <label className="block mb-2 text-sm font-bold text-gray-700" for="lastName">
+                                                Profile Picture
                                             </label>
                                             <input
-                                                class="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                                                className="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
                                                 id="lastName"
                                                 type="text"
-                                                placeholder="Last Name"
+                                                placeholder="Profile Picture Link"
                                             />
                                         </div>
                                     </div>
-                                    <div class="mb-4">
-                                        <label class="block mb-2 text-sm font-bold text-gray-700" for="email">
-                                            Email
-                                        </label>
-                                        <input
-                                            class="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
-                                            id="email"
-                                            type="email"
-                                            placeholder="Email"
-                                        />
-                                    </div>
-                                    <div class="mb-4 md:flex md:justify-between">
-                                        <div class="mb-4 md:mr-2 md:mb-0">
-                                            <label class="block mb-2 text-sm font-bold text-gray-700" for="password">
-                                                Password
-                                            </label>
-                                            <input
-                                                class="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border border-red-500 rounded shadow appearance-none focus:outline-none focus:shadow-outline"
-                                                id="password"
-                                                type="password"
-                                                placeholder="******************"
-                                            />
-                                            <p class="text-xs italic text-red-500">Please choose a password.</p>
-                                        </div>
-                                        <div class="md:ml-2">
-                                            <label class="block mb-2 text-sm font-bold text-gray-700" for="c_password">
-                                                Confirm Password
-                                            </label>
-                                            <input
-                                                class="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
-                                                id="c_password"
-                                                type="password"
-                                                placeholder="******************"
-                                            />
-                                        </div>
-                                    </div>
-                                    <div class="mb-6 text-center">
-                                        <button
-                                            class="w-full px-4 py-2 font-bold text-white bg-blue-500 rounded-full hover:bg-blue-700 focus:outline-none focus:shadow-outline"
-                                            type="button"
-                                        >
-                                            Register Account
-                                        </button>
-                                    </div>
-                                    <hr class="mb-6 border-t" />
-                                    <div class="text-center">
-                                        <a
-                                            class="inline-block text-sm text-blue-500 align-baseline hover:text-blue-800"
-                                            href="#a"
-                                        >
-                                            Forgot Password?
-                                        </a>
-                                    </div>
-                                    <div class="text-center">
-                                        <a
-                                            class="inline-block text-sm text-blue-500 align-baseline hover:text-blue-800"
-                                            href="./index.html"
-                                        >
-                                            Already have an account? Login!
-                                        </a>
-                                    </div>
-                                </form>
-                            </div>
+                                </div>
+
+                                <div className="mb-4">
+                                    <label className="block mb-2 text-sm font-bold text-gray-700" for="email">
+                                        Write a Blog
+                                    </label>
+                                    <textArea
+                                        className="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                                        placeholder="Write here ..."
+                                        rows={3}
+                                    >
+
+                                    </textArea>
+                                </div>
+                                <div className="mb-6 text-center">
+                                    <button
+                                        className="px-4 py-2 text-white bg-gray-600 rounded hover:bg-gray-800 focus:outline-none focus:shadow-outline"
+                                        type="button">
+                                        Submit
+                                    </button>
+                                </div>
+                            </form>
                         </div>
                     </div>
                 </div>
-            </body>
-        </main>
+            </div>
+        </main >
     );
 };
 
